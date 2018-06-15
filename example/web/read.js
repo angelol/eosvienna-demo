c90e00ebbfaf5a2390ab7bfb739d68950ae9753e
Angelo Laub <angelolaub@gmail.com>
2018-06-15 13:45:14 +0200

    getTableRows can get data by primary key like this

--- example/web/read.js
+++ example/web/read.js
@@ -2,8 +2,8 @@ var rows;
 $(function() {
 	
 	let id = getUrlParameter("id");
-    eos.getTableRows({json:true, scope: account, code: contract,  table: 'blog', limit:100}).then(res => {
-		let row = res.rows.filter(x => x.id == id)[0];
+    eos.getTableRows({json:true, scope: account, code: contract,  table: 'blog', limit:1, lower_bound: id}).then(res => {
+		let row = res.rows[0];
 		
 		$("h1").text(row.title);
 		$("div.author").text(row.author);
